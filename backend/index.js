d347211f459ecfedb33368aada5db91055dd16b5
ShokatShahinjsx <shokatshahin50@gmail.com>
2025-05-29 00:52:55 +0400

    New Big Changes

--- backend/index.js
+++ backend/index.js
@@ -29,10 +29,10 @@ app.use("/api/expenses", expensesRoute);
 app.use("/api/users", userRouter);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
-
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+  
   app.get("/*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../frontend", "dist", "index.html"));
+    res.sendFile(path.resolve(__dirname, "/frontend", "dist", "index.html"));
   });
 }
 
